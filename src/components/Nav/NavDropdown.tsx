fe9e1b2cf6b4ad809e37c7ecf303011301c09f48
Muhammad Nur <md.nurealamsiddiquee2004@gmail.com>
2025-01-15 18:54:32 +0600

    dropdown new issue solved

--- src/components/Nav/NavDropdown.tsx
+++ src/components/Nav/NavDropdown.tsx
@@ -7,19 +7,11 @@ const NavDropdown = ({
   name: string;
   routes: { name: string; url?: string }[];
 }) => {
-  const handleVisible = () => {
-    const dropNav = document.getElementById("dropNav");
-    if (dropNav && dropNav.classList.contains("hidden")) {
-      dropNav.classList.remove("hidden");
-    } else {
-      dropNav?.classList.add("hidden");
-    }
-  };
   return (
     <li className="dropdown dropdown-hover z-30">
       <details>
         <summary className="uppercase">{name}</summary>
-        <ul id="dropNav" onClick={handleVisible} className="p-2 w-56 bg-base-200">
+        <ul className="p-2 w-56 bg-base-200">
           {routes.map((route, index) => (
             <NavLink key={index} name={route.name} route={route.url} />
           ))}
