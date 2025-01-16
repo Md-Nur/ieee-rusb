60855750e079cf94eb246e600a92c49687f6590e
Muhammad Nur <md.nurealamsiddiquee2004@gmail.com>
2025-01-17 04:01:00 +0600

    user id param problem solved

--- src/app/api/users/[id]/route.ts
+++ src/app/api/users/[id]/route.ts
@@ -1,7 +1,11 @@
 import dbConnect from "@/lib/dbConnect";
 import UserModel from "@/models/user.model";
 
-export async function GET({ params: { id } }: { params: { id: string } }) {
+export async function GET(
+  req: Request,
+  { params }: { params: Promise<{ id: string }> }
+) {
+  const { id } = await params;
   await dbConnect();
   const user = await UserModel.findById(id);
   if (!user) {
