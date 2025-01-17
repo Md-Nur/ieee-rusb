1985a4205f19cca3f389cdc2e2ac19b9c7d092e6
Muhammad Nur <md.nurealamsiddiquee2004@gmail.com>
2025-01-17 19:05:52 +0600

    img resize

--- src/components/Home/Chairperson.tsx
+++ src/components/Home/Chairperson.tsx
@@ -21,21 +21,21 @@ const Chairperson = () => {
   }, []);
   return (
     <div className="card card-side bg-base-300 shadow-xl max-w-7xl mx-auto my-20 flex-wrap">
-      <figure className="card-body md:max-h-96 h-full w-full md:max-w-96 m-0 p-0 rounded-xl">
-        <Image
+      <figure className="card-body md:max-h-96 h-full w-full md:max-w-96 p-3 m-3 sm:m-0 sm:p-0 rounded-xl">
+        <img
           src={chair?.avatar || "/billaVai.jpg"}
           alt="Chairperson"
           width={300}
           height={300}
-          className="object-cover"
+          className="object-cover rounded-xl md:rounded-none"
         />
       </figure>
       <div className="card-body w-[calc(100%-768px)]">
         <h2 className="text-2xl font-bold">{chair?.name}</h2>
-        <p className="text-xl mt-0">{chair?.position || "Founder"}</p>
+        <p className="text-xl mt-0">{chair?.position || "Chairperson"}</p>
         <p className="text-justify">
           The IEEE Rajshahi University Student Branch (RU SB) was founded in
-          July 2017. <br /> The IEEE RU SB is a student branch of the Institute
+          July 2017. <br /> The IEEE RUSB is a student branch of the Institute
           of Electrical and Electronics Engineers (IEEE). The IEEE was formed in
           1963 when the American Institute of Electrical Engineers (AIEE) and
           the Institute of Radio Engineers (IRE) merged. The IEEE RU SB&apos;s
